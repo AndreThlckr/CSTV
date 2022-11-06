8489fefd90d17a1ec8980004ce3e708109eb902e
André Theilacker <andretheilacker@gmail.com>
2022-11-06 16:57:52 -0300

    Added splashscreen

--- app/src/main/java/io/github/andrethlckr/cstv/core/ui/MainActivity.kt
+++ app/src/main/java/io/github/andrethlckr/cstv/core/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package io.github.andrethlckr.cstv.core.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.andrethlckr.cstv.core.ui.theme.CSTVTheme
 
@@ -11,6 +12,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
 
         setContent {
             CSTVTheme {
